40429de8408cd0a15d1d207271087e5183c8f8b7
hamzboy99 <31826655+hamzboy99@users.noreply.github.com>
2017-11-10 14:12:54 +0100

    Delete e4.1.py

--- extra opdrachten/e4.1.py
+++
@@ -1,7 +0,0 @@
-invoer= eval(input('Welke temeratuur is het vandaag: '))
-if invoer <= 0:
-    print('Het vriest vandaag')
-elif invoer >=0 and invoer <= 15:
-    print('Het is koud vandaag')
-else:
-    print('Het is lekker weer vandaag ')
